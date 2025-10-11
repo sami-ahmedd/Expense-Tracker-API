e3ced7ec8d81f6ce9d4bea1f55699d17c301c7d7
Sami Ahmed <syedsami2005@gmail.com>
2025-10-11 16:22:17 +0530

    added .env

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express  = require('express');
+const dotenv =require('dotenv');
+dotenv.config(); 
+
 const app = express();
+
+const port = process.env.PORT || 3000;
 
 app.get('/', (request , response) => response.send('Home page'));
 
-const PORT = 3000;
-app.listen(PORT || 3000 ,console.log(`listening on port ${PORT}`));
+
+app.listen(port ,()=>  console.log(`listening on port ${port}`));
