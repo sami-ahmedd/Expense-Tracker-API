e7966c3a502a0eae82533e1d5dbca99a2396015b
Sami Ahmed <syedsami2005@gmail.com>
2025-10-11 15:50:35 +0530

    moved package json to src

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express  = require('express');
 const app = express();
 
+app.get('/', (request , response) => response.send('Home page'));
 
 const PORT = 3000;
 app.listen(PORT || 3000 ,console.log(`listening on port ${PORT}`));
