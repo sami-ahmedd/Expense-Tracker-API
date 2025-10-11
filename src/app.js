ed6716b6ddc4a139fe6b479342282a232cc0f9f3
Sami Ahmed <syedsami2005@gmail.com>
2025-10-11 18:11:44 +0530

    connected to mongo database

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express  = require('express');
 const dotenv =require('dotenv').config();
-
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI )
+    .then(() => console.log('connected to database'))
+    .catch((err) => console.log(err))
 
 const port = process.env.PORT || 3000;
 
