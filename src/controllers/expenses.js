51ec06c57ff0b025a621018a92662f9bbad00c33
Sami Ahmed <syedsami2005@gmail.com>
2025-10-12 06:51:49 +0530

    added comments

--- src/controllers/expenses.js
+++ src/controllers/expenses.js
@@ -1,24 +1,24 @@
-//
-//
+//@desc create new expense
+//@route POST /api/expenses
 const createExpense = (request,response) => {
     response.send({message : 'create expense'})
 }
 
-//@dest get all expenses
+//@desc get all expenses
 //@route GET /api/expenses
 const getAllExpenses =  (request,response) => {
     response.send('all the expenses you have made');
 };
 
-//@dest update expense with id
+//@desc update expense with id
 //@route PATCH /api/expenses/:id
 const updateExpense = (request,response) => {
     const {body , params : {id}} = request;
     response.send({message : `updating expense with id : ${id}`})
 }
 
-//
-//
+//@desc update expense with id
+//@route PATCH /api/expenses/:id
 const deleteExpense =  (request,response) => {
     const {params : {id}} = request;
     response.send({message : `deleting expense with id : ${id}`});
