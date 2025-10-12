225e21beb534361609aae4bbb0cf409d0d94d46c
Sami Ahmed <syedsami2005@gmail.com>
2025-10-12 14:37:37 +0530

    all basic crud routes working

--- src/controllers/expenses.js
+++ src/controllers/expenses.js
@@ -3,29 +3,48 @@ const {expenseModel} = require('../models/expenseModel')
 //@desc create new expense
 //@route POST /api/expenses
 const createExpense = async (request,response) => {
-    const {body} = request
-    const expense = await expenseModel.create(body);
-    response.status(201).send({expense})
+    
+    try {
+        const expense = await expenseModel.create(request.body);
+        response.status(200).send({expense})
+    } catch (error) {
+        response.status(500).send({msg : error})
+    }
 }
 
 //@desc get all expenses
 //@route GET /api/expenses
-const getAllExpenses =  (request,response) => {
-    response.send('all the expenses you have made');
+const getAllExpenses = async (request,response) => {
+    try {
+        const allExpenses = await expenseModel.find()
+        response.status(200).send({allExpenses})
+    } catch (error) {
+         response.status(500).send({msg : error})
+    }
 };
 
 //@desc update expense with id
 //@route PATCH /api/expenses/:id
-const updateExpense = (request,response) => {
+const updateExpense = async (request,response) => {
     const {body , params : {id}} = request;
-    response.send({message : `updating expense with id : ${id}`})
+    const findExpense = await expenseModel.findByIdAndUpdate(id,body)
+    response.status(200).send({findExpense})
+
 }
 
 //@desc update expense with id
 //@route PATCH /api/expenses/:id
-const deleteExpense =  (request,response) => {
-    const {params : {id}} = request;
-    response.send({message : `deleting expense with id : ${id}`});
+const deleteExpense =  async (request,response) => {
+    try {
+        const {params : {id}} = request;
+        const deletedExpense = await expenseModel.findByIdAndDelete(id);
+        if(!deletedExpense){
+            return response.status(404).send({msg: `no task with id ${id}`})
+        }
+        response.status(200).send({deletedExpense})
+    } catch (error) {
+        response.status(500).send({msg: error})
+    }
 }
 
 module.exports = {getAllExpenses , updateExpense , createExpense , deleteExpense};
