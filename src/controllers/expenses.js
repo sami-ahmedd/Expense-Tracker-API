ea70aa5e22fd453f79f1b54be3b375bc46b021d1
Sami Ahmed <syedsami2005@gmail.com>
2025-10-13 12:15:47 +0530

    fixed bug when filter is blank

--- src/controllers/expenses.js
+++ src/controllers/expenses.js
@@ -1,4 +1,4 @@
-const { body } = require('express-validator');
+const { body , query , param , validationResult} = require('express-validator');
 const {expenseModel} = require('../models/expenseModel')
 //@desc create new expense
 //@route POST /api/expenses
