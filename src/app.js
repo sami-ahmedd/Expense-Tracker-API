0ced2e5bbb60d2b7868010715138a502559cf596
Sami Ahmed <syedsami2005@gmail.com>
2025-10-13 17:09:20 +0530

    created users schema

--- src/app.js
+++ src/app.js
@@ -2,16 +2,28 @@ const express  = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const expensesRouter = require('./routes/expenses')
-
+const session = require('express-session')
 const app = express();
 
 app.use(express.json());
+
+app.use(session({
+    secret : 'keyboard cat dog',
+    resave : false,
+    saveUninitialized : false, 
+    cookie : {
+        secure : true , 
+        maxAge : 60000 * 60 * 60
+    }
+}));
+
+app.use()
+
 app.use(expensesRouter);
 
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (request , response) => response.send('Home page'));
 
 mongoose.connect(process.env.MONGO_URI )
     .then(() => {
---
+++ src/models/users.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+mongoose.schemas
+let userSchema = mongoose.Schema({
+    username : {
+        type : String ,
+        required  : [true, 'must provide username']  ,
+        unique : [true , 'username already exists , please try another one']
+    },
+    password : {
+        type : String ,
+        required : [true, 'must provide password'] 
+    },
+    email : {
+        type : String ,
+        required : true ['must provide email'] , 
+    }
+})
+
+let userModel = mongoose.model('userModel' , userSchema );
+module.expors = {userModel}
